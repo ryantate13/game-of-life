b9ccf22185b8c3c9ca36e53344634130679d9b89
Ryan Tate <ryantate13@gmail.com>
2020-09-20 01:22:19 -0700

    setup gh-pages

---
+++ precache-manifest.b4568caa13c8155fb5b75fec22a51104.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "553f2da18fbf498758e781d869f37bc4",
+    "url": "/game-of-life/index.html"
+  },
+  {
+    "revision": "acdaecf16a4dbe351600",
+    "url": "/game-of-life/static/css/main.2d2db81f.chunk.css"
+  },
+  {
+    "revision": "5ded8c3601937bc35db4",
+    "url": "/game-of-life/static/js/2.f983c4e0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/game-of-life/static/js/2.f983c4e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acdaecf16a4dbe351600",
+    "url": "/game-of-life/static/js/main.35d3712a.chunk.js"
+  },
+  {
+    "revision": "a6893dc5f1659096e143",
+    "url": "/game-of-life/static/js/runtime-main.1dafd470.js"
+  }
+]);
